8b8cf95a2506069043b3557d136b49703338bd7e
kanakana-rcj <nakagawa.dadada5@gmail.com>
2024-12-21 22:24:50 +0900

    add link-card

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rlc from "remark-link-card";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kanakana-rcj.github.io',
-	integrations: [mdx(), sitemap()]
+	integrations: [mdx(), sitemap()],
+
+	markdown: {
+		remarkPlugins: [rlc]
+	}
 });
