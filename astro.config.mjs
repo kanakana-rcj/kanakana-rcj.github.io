4c226cdead75c52793d2ca661bf5a3ee871db8ee
kanakana-rcj <nakagawa.dadada5@gmail.com>
2024-12-23 21:20:09 +0900

    introduce google analytics4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import rlc from "remark-link-card";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kanakana-rcj.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 
 	markdown: {
 		remarkPlugins: [rlc]
